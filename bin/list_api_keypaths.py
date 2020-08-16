2ca5612759cf32589ca6726cb2a88622b49e8133
Wayne Schmidt <wschmidt@sumologic.com>
2020-08-17 07:56:41 +0900

    removed coomments

--- bin/list_api_keypaths.py
+++ bin/list_api_keypaths.py
@@ -19,10 +19,4 @@ yaml_stream = requests.get(API_URL).text
 yaml_dict = benedict.from_yaml(yaml_stream)
 
 for keypath in benedict.keypaths(yaml_dict):
-    if 'info.version' in keypath:
-        my_dict = yaml_dict[keypath]
-        print('### API_Version: {}'.format(my_dict))
-    if 'info.name' in keypath:
-        my_dict = yaml_dict[keypath]
-        print('### API_NAME: {}'.format(my_dict))
     print('{}'.format(keypath))
